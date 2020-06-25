376dde41e4245554534432019c7fa00efd1e306d
seungbin <tanja1206@naver.com>
2020-06-25 22:21:27 +0900

    init

--- src/models/index.js
+++ src/models/index.js
@@ -1,12 +1,24 @@
-const path = require('path');
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
-const config = require(path.join(__dirname + '..', 'config', 'config.json'))[env];
-
+const config = require('../config/config.json')[env];
 const db = {};
+
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+
+db.User = require('./user')(sequelize, Sequelize);
+db.Todo = require('./todo')(sequelize, Sequelize);
+db.Goal = require('./goal')(sequelize, Sequelize);
+db.Stone = require('./stone')(sequelize, Sequelize);
+db.phrase = require('./phrase')(sequelize, Sequelize);
+
+db.User.hasMany(db.Todo);
+db.Todo.belongsTo(db.User);
+db.User.hasOne(db.Stone);
+db.Stone.belongsTo(db.User);
+db.User.hasOne(db.Goal);
+db.Goal.belongsTo(db.User);
 
 module.exports = db;
