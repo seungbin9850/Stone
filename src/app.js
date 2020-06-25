9b0d2427c26478e2c589da979caeceb90ed6e1c7
seungbin <tanja1206@naver.com>
2020-06-25 22:20:26 +0900

    init

--- src/app.js
+++ src/app.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const { sequelize } = require('./models');
-const logger = requier("morgan");
+const logger = require("morgan");
+const YAML = require('yamljs');
+const swaggerUi = require('swagger-ui-express');
 
 const app = express();
 
 require('dotenv').config();
+const swaggerDoc = YAML.load('./apidocs/swagger.yaml');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(logger('dev'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
 
 app.set('jwt-secret', process.env.JWT_SECRET);
 
 sequelize.sync();
 
-app.listen(3000, () => {
+app.listen(8080, () => {
     console.log("Server On");
 });
