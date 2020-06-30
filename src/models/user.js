ebe77587d752a09aa53e7eee3ad2b8ebf3905cbf
seungbin <tanja1206@naver.com>
2020-06-30 22:13:02 +0900

    [ADD] models

--- src/models/user.js
+++ src/models/user.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
         },
         auto: {
             type: DataTypes.BOOLEAN,
-            defaultValue: true,
+            defaultValue: false,
         }
     });
 }
