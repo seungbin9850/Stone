d342cfbe42454a5f0a6f3ac58ea63b94e7d18739
seungbin <tanja1206@naver.com>
2020-07-02 22:13:18 +0900

    init

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const { sequelize } = require('./models');
 const logger = require("morgan");
 const YAML = require('yamljs');
 const swaggerUi = require('swagger-ui-express');
+const cors = require('cors');
 
 const app = express();
 
@@ -11,12 +12,15 @@ const swaggerDoc = YAML.load('./apidocs/swagger.yaml');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
 app.use(logger('dev'));
 app.use('/', require('./routes'));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
 
 app.set('jwt-secret', process.env.JWT_SECRET);
+app.set('refresh-secret', process.env.REFRESH_SECRET);
+app.set('crypto-secret', process.env.CRYPTO_SECRET);
 
 sequelize.sync();
 
