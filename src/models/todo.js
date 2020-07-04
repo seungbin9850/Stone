39c0df4ea0d1de3e6884bd10b056e5134a54f2c0
seungbin <tanja1206@naver.com>
2020-07-04 21:43:58 +0900

    [ADD] models

--- src/models/todo.js
+++ src/models/todo.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.INTEGER,
             allowNull: false,
         },
-        do: {
+        what: {
             type: DataTypes.STRING(200),
             allowNull: false,
         },
