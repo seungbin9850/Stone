9a0cbff731b28af94b0212b0449d63f437fbb930
seungbin <tanja1206@naver.com>
2020-07-10 09:12:07 +0900

    [EDIT] token check

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,17 +1,40 @@
 const jwt = require('jsonwebtoken');
 
-module.exports = (req, res, next) => {
+// module.exports = (req, res, next) => {
+//     const token = req.headers['access_token'];
+//     const secret = req.app.get('jwt-secret');
+//     if (token) {
+//         jwt.verify(token, secret, (err, decoded) => {
+//             if (err) throw new Error(err);
+//             req.decoded = decoded;
+//             next();
+//         });
+//     } else {
+//         res.status(403).json({
+//             message: "로그인 되어있지 않음"
+//         })
+//     }
+// }
+
+const authMiddleware = async (req, res, next) => {
     const token = req.headers['access_token'];
-    const secret = req.app.get('jwt-secret');
-    if (token) {
-        jwt.verify(token, secret, (err, decoded) => {
-            if (err) throw new Error(err);
-            req.decoded = decoded;
-            next();
-        });
-    } else {
+    if (!token) {
         res.status(403).json({
             message: "로그인 되어있지 않음"
         })
     }
+
+    try {
+        await jwt.verify(token, req.app.get('jwt-secret'), (err, decoded) => {
+            if (err) throw new Error(err.message);
+            req.decoded = decoded;
+            next();
+        })
+    } catch (err) {
+        res.status(403).json({
+            message: err.message
+        })
+    }
 }
+
+module.exports = authMiddleware;
