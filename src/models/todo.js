6eea678f98f117df2957db977acd1ca815b7cf55
seungbin <tanja1206@naver.com>
2020-07-10 09:11:36 +0900

    [EDIT] todo model

--- src/models/todo.js
+++ src/models/todo.js
@@ -1,30 +1,8 @@
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define('todo', {
-        year: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        month: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        day: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
         what: {
             type: DataTypes.STRING(200),
             allowNull: false,
-        },
-        check: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false,
-        },
-        time: {
-            type: DataTypes.INTEGER,
-        },
-        minute: {
-            type: DataTypes.INTEGER,
         }
     });
 }
