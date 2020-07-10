c9422733dcb29c4186c04725a1741293d35c6798
seungbin <tanja1206@naver.com>
2020-07-10 09:13:05 +0900

    [ADD] no todo

--- src/routes/main/controller.js
+++ src/routes/main/controller.js
@@ -1,4 +1,4 @@
-const { User, Stone, Phrase, sequelize } = require('../../models');
+const { User, Stone, Phrase, sequelize, Todo } = require('../../models');
 
 const showMain = async (req, res, next) => {
     const id = req.decoded.id;
@@ -12,6 +12,22 @@ const showMain = async (req, res, next) => {
         const stone = await Stone.findOne({
             userId: id
         });
+        const todo = await Todo.findOne({
+            userId: id
+        });
+        if (todo) {
+            res.status(200).json({
+                name: user.name,
+                hour: user.hour,
+                minute: user.minute,
+                level: stone.level,
+                growth: stone.growth,
+                phrase_name: phrase.name,
+                phrase_word: phrase.word,
+                todo_what: todo.what.split('^-;%&'),
+                message: "성공"
+            })
+        }
         res.status(200).json({
             name: user.name,
             hour: user.hour,
@@ -23,7 +39,7 @@ const showMain = async (req, res, next) => {
             message: "성공"
         })
     } catch (err) {
-        res.status(500).json({
+        res.status(404).json({
             message: err.message
         })
     }
