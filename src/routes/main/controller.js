a2449877ec37d60322469e7cfef63860549add07
seungbin <tanja1206@naver.com>
2020-07-11 20:10:15 +0900

    [ADD] show main

--- src/routes/main/controller.js
+++ src/routes/main/controller.js
@@ -3,17 +3,23 @@ const { User, Stone, Phrase, sequelize, Todo } = require('../../models');
 const showMain = async (req, res, next) => {
     const id = req.decoded.id;
     try {
-        const phrase = await Phrase.findOne({
-            order: sequelize.random()
-        });
         const user = await User.findOne({
-            id
+            where: {
+                id
+            }
         });
         const stone = await Stone.findOne({
-            userId: id
+            where: {
+                userId: id
+            }
         });
         const todo = await Todo.findOne({
-            userId: id
+            where: {
+                userId: id
+            }
+        });
+        const phrase = await Phrase.findOne({
+            order: sequelize.random()
         });
         if (todo) {
             res.status(200).json({
@@ -27,21 +33,22 @@ const showMain = async (req, res, next) => {
                 todo_what: todo.what.split('^-;%&'),
                 message: "성공"
             })
+        } else {
+            res.status(200).json({
+                name: user.name,
+                hour: user.hour,
+                minute: user.minute,
+                level: stone.level,
+                growth: stone.growth,
+                phrase_name: phrase.name,
+                phrase_word: phrase.word,
+                message: "성공"
+            })
         }
-        res.status(200).json({
-            name: user.name,
-            hour: user.hour,
-            minute: user.minute,
-            level: stone.level,
-            growth: stone.growth,
-            phrase_name: phrase.name,
-            phrase_word: phrase.word,
-            message: "성공"
-        })
     } catch (err) {
         res.status(404).json({
             message: err.message
-        })
+        }).end();
     }
 }
 
