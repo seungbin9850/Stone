ae2b10944b656cd9c4447ee930c083aaead869c8
seungbin <tanja1206@naver.com>
2020-07-11 20:09:44 +0900

    [ADD] token check

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,29 +1,12 @@
 const jwt = require('jsonwebtoken');
 
-// module.exports = (req, res, next) => {
-//     const token = req.headers['access_token'];
-//     const secret = req.app.get('jwt-secret');
-//     if (token) {
-//         jwt.verify(token, secret, (err, decoded) => {
-//             if (err) throw new Error(err);
-//             req.decoded = decoded;
-//             next();
-//         });
-//     } else {
-//         res.status(403).json({
-//             message: "로그인 되어있지 않음"
-//         })
-//     }
-// }
-
 const authMiddleware = async (req, res, next) => {
     const token = req.headers['access_token'];
     if (!token) {
         res.status(403).json({
             message: "로그인 되어있지 않음"
-        })
+        }).end();
     }
-
     try {
         await jwt.verify(token, req.app.get('jwt-secret'), (err, decoded) => {
             if (err) throw new Error(err.message);
@@ -33,7 +16,7 @@ const authMiddleware = async (req, res, next) => {
     } catch (err) {
         res.status(403).json({
             message: err.message
-        })
+        }).end();
     }
 }
 
