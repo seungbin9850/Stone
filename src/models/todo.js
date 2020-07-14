0dae7ff8fef17d579f595d3d5898e340c6779779
seungbin <tanja1206@naver.com>
2020-07-14 17:15:32 +0900

    [ADD] todo default value

--- src/models/todo.js
+++ src/models/todo.js
@@ -2,6 +2,7 @@ module.exports = (sequelize, DataTypes) => {
     return sequelize.define('todo', {
         what: {
             type: DataTypes.STRING(200),
+            defaultValue: null
         }
     });
 }
