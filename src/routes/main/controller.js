63c9d596552243acff9900e2f59399a6cf57a55f
seungbin <tanja1206@naver.com>
2020-08-31 21:11:27 +0900

    main controller

--- src/routes/main/controller.js
+++ src/routes/main/controller.js
@@ -1,58 +1,61 @@
-const { User, Stone, Phrase, sequelize, Todo } = require('../../models');
+const { User, Stone, Phrase, sequelize, Todo } = require("../../models");
 
 const showMain = async (req, res, next) => {
-    const id = req.decoded.id;
-    try {
-        const user = await User.findOne({
-            where: {
-                id
-            }
-        });
-        const stone = await Stone.findOne({
-            where: {
-                userId: id
-            }
-        });
-        const todo = await Todo.findOne({
-            where: {
-                userId: id
-            }
-        });
-        const phrase = await Phrase.findOne({
-            order: sequelize.random()
-        });
-        if (todo.what) {
-            res.status(200).json({
-                name: user.name,
-                hour: user.hour,
-                minute: user.minute,
-                level: stone.level,
-                growth: stone.growth,
-                phrase_name: phrase.name,
-                phrase_word: phrase.word,
-                todo_what: todo.what.split('^-;%&'),
-                message: "성공"
-            })
-        } else {
-            res.status(200).json({
-                name: user.name,
-                hour: user.hour,
-                minute: user.minute,
-                level: stone.level,
-                growth: stone.growth,
-                phrase_name: phrase.name,
-                phrase_word: phrase.word,
-                todo_what: [],
-                message: "성공"
-            })
-        }
-    } catch (err) {
-        res.status(404).json({
-            message: err.message
-        }).end();
+  const id = req.decoded.id;
+  try {
+    const user = await User.findOne({
+      where: {
+        id,
+      },
+    });
+    const stone = await Stone.findOne({
+      where: {
+        userId: id,
+      },
+    });
+    const todo = await Todo.findOne({
+      where: {
+        userId: id,
+      },
+    });
+    const phrase = await Phrase.findOne({
+      order: sequelize.random(),
+    });
+    if (todo.what) {
+      res.status(200).json({
+        name: user.name,
+        hour: user.hour,
+        minute: user.minute,
+        level: stone.level,
+        growth: stone.growth,
+        phrase_name: phrase.name,
+        phrase_word: phrase.word,
+        todo_what: todo.what.split("^-;%&"),
+        message: "성공",
+      });
+    } else {
+      res.status(200).json({
+        name: user.name,
+        hour: user.hour,
+        minute: user.minute,
+        level: stone.level,
+        growth: stone.growth,
+        phrase_name: phrase.name,
+        phrase_word: phrase.word,
+        todo_what: [],
+        message: "성공",
+      });
     }
-}
+  } catch (err) {
+    res
+      .status(404)
+      .json({
+        message: err.message,
+      })
+      .end();
+  }
+};
 
 module.exports = {
-    showMain
-}
+  showMain,
+};
