4c6801bc7f51608e550a30c8eaf05c3fc24bc97a
seungbin <tanja1206@naver.com>
2020-09-01 21:33:40 +0900

    diary controller

--- src/routes/diary/controller.js
+++ src/routes/diary/controller.js
@@ -1,26 +1,26 @@
-const { Diary } = require('../../models');
+const { Diary } = require("../../models");
 
 const write = async (req, res, next) => {
-    const { year, month, day, content } = req.body;
-    const userId = req.decoded.id;
-    try {
-        await Diary.create({
-            year,
-            month,
-            day,
-            content,
-            userId
-        });
-        res.status(200).json({
-            message: "성공"
-        })
-    } catch (err) {
-        res.status().json({
-            message: "실패"
-        })
-    }
-}
+  const { year, month, day, content } = req.body;
+  const userId = req.decoded.id;
+  try {
+    await Diary.create({
+      year,
+      month,
+      day,
+      content,
+      userId,
+    });
+    res.status(200).json({
+      message: "성공",
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "실패",
+    });
+  }
+};
 
 module.exports = {
-    write
-}
+  write,
+};
